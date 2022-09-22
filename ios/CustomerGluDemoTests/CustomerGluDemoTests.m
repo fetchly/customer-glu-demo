520a0ea0fa5958cbc6fd5adcde1afdea25c98c54
Luke S <lucassilva@gmail.com>
2022-09-22 12:52:36 -0300

    rename the app to CustomerGluDemo

--- ios/CustomerGluDemoTests/CustomerGluDemoTests.m
+++ ios/CustomerGluDemoTests/CustomerGluDemoTests.m
@@ -7,11 +7,11 @@
 #define TIMEOUT_SECONDS 600
 #define TEXT_TO_LOOK_FOR @"Welcome to React"
 
-@interface AmusoftechTests : XCTestCase
+@interface CustomerGluDemoTests : XCTestCase
 
 @end
 
-@implementation AmusoftechTests
+@implementation CustomerGluDemoTests
 
 - (BOOL)findSubviewInView:(UIView *)view matching:(BOOL(^)(UIView *view))test
 {
